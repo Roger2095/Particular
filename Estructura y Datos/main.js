786cbb9e2157e6e17cba70e538d41f21e5559336
Roger <roger.spioussas@outlook.com.ar>
2024-08-04 05:39:31 -0300

    Datos primitivos

---
+++ Estructura y Datos/main.js
@@ -0,0 +1,32 @@
+// Tipos de datos
+
+//Number
+16 - 54;
+NaN;
+
+//Strings
+("Hola");
+("16");
+"Chau"`Backticks`;
+
+//Booleans
+true;
+false;
+
+//Undefined
+
+//null
+
+//Arrays
+
+//Funciones
+
+//Objetos
+
+//Dentro de un Formulario
+
+//Nombre - String
+//Apellido - String
+//Edad - Number
+//Estudias en Glaobal? - Booleans
+//Que modulos vas a ver? - Arrays
