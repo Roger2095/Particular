2c25520a014043ff0818c352f3658c446eb1aba7
Roger <roger.spioussas@outlook.com.ar>
2024-08-04 07:01:37 -0300

    Scope y Hosting

---
+++ Scope y Hosting/main.js
@@ -0,0 +1,20 @@
+{
+  var a = "var";
+}
+console.log(a);
+
+{
+  let b = "let";
+  console.log(b);
+}
+
+//console.log(b);
+
+{
+  const c = "const";
+  console.log(c);
+}
+
+//console.log(c);
+
+//solamente "var" es la unica variblae que se puede leer global en cambio "let" y "const", solamente a nivel local, dentro de las llaves
