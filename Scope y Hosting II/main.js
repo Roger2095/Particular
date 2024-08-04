81531256c1a8b812bb15be03c13080d2395db343
Roger <roger.spioussas@outlook.com.ar>
2024-08-04 07:19:57 -0300

    Scope y Hosting II

---
+++ Scope y Hosting II/main.js
@@ -0,0 +1,13 @@
+console.log(primervariable);
+
+var primervariable = "Hola soy la primer variable";
+
+console.log(primervariable);
+
+{
+  var segundavariblae = "hola soy la segunda variable";
+}
+
+console.log(segundavariblae);
+
+// solamente la variable var se puede leer a nivel globlal, tambien al principio como undefined a pesar que no esta aun declarado pero si busca una respuesta segundo el valor que buscamos, tanto dentro o fuera del bloque. En cambio con "let" y "const" no se puede leer por afuera del bloque scope.
